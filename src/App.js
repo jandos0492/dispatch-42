6c90024c1fd5a307dfdef814da950c3f8950abcf
jandos0492 <jandos0492@gmail.com>
2024-05-25 02:35:49 -0400

    Added the NavBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello world</div>
-    </div>
-  );
+    <>
+      <NavBar />
+    </>
+  )
 }
 
 export default App;
